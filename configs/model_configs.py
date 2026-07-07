5c772fc6b082822187c045175205df3664998f94
Mariia Kobycheva <mkobycheva@kse.org.ua>
2026-07-07 21:38:27 +0300

    Align ALS branch with shared baseline rules

--- configs/model_configs.py
+++ configs/model_configs.py
@@ -1,10 +1,16 @@
 """Model hyperparameter configurations."""
 
 SVD_CONFIG = {}
-ALS_CONFIG = {}
+ALS_CONFIG = {
+    "factors": 128,
+    "regularization": 0.05,
+    "iterations": 20,
+    "alpha": 15,
+    "k": 10,
+    "random_state": 42,
+}
 ITEM2VEC_CONFIG = {}
 LSTM_CONFIG = {}
 SASREC_CONFIG = {}
 BERT4REC_CONFIG = {}
 ENSEMBLE_CONFIG = {}
-
